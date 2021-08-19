3102e8af20d69cccb331007b370a4d1f8bb93c99
Jack Ruan <jackruan99@gmail.com>
2021-08-19 08:39:32 -0700

    update

--- tests/expected_value.py
+++ tests/expected_value.py
@@ -13,7 +13,7 @@ from game.simulator import get_chips_after_rounds
 # Without betting deviation: 100/100 -> 12 seconds
 # With betting deviation: 100/100 -> 35 seconds
 number_of_rounds = 100
-number_of_games = 10000
+number_of_games = 100
 chips = []
 for _ in range(number_of_games):
     chips.append(get_chips_after_rounds(number_of_rounds))
@@ -24,6 +24,8 @@ print(f'Expected Chips: {sum(chips) / number_of_games}')
 
 # No Betting and Playing Deviation (bet amount: 100)
 # rounds: 100, games: 10000 => expected chips: 996.5417
+# rounds: 100, games: 100000 => expected chips: 1003.76466
 
 # With Betting Deviation (bet amount: <0.5 - 10, <1 - 50, <1.5 - 100, ..., <5.5 - 500, <6 - 550, >=6 - 600)
 # rounds: 100, games: 10000 => expected chips: 1030.5623
+# rounds: 100, games: 100000 => expected chips: 1030.32981
