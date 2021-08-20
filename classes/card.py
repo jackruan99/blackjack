30616e494b977de64d24fc8b9cb6d9ce5eed5caa
Jack Ruan <jackruan99@gmail.com>
2021-08-20 12:15:16 -0700

    update

--- classes/card.py
+++ classes/card.py
@@ -2,7 +2,6 @@ class Card:
     def __init__(self, rank, suit, shown=True):
         self.rank = rank
         self.suit = suit
-        self.value = 10 if rank > 10 else rank
         self.shown = shown
 
     # ace = 1, ... , 10 = 10, jack = 11, queen = 12, king = 13
@@ -13,10 +12,6 @@ class Card:
     def get_suit(self):
         return self.suit
 
-    # ace = 1, ... , 10 = 10, jack = 10, queen = 10, king = 10
-    def get_value(self):
-        return self.value
-
     def get_shown(self):
         return self.shown
 
@@ -25,6 +20,10 @@ class Card:
 
     def flip(self):
         self.shown = not self.shown
+    
+    # ace = 1, ... , 10 = 10, jack = 10, queen = 10, king = 10
+    def get_value(self):
+        return 10 if self.rank > 10 else self.rank
 
     def get_name(self):
         if not self.shown:
